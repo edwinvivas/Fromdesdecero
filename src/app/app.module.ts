7192de747be6009e8b56231102f8c8a2b85b3cc0
Edwin Vivas <edwinvivassoto@gmail.com>
2019-09-22 18:25:57 -0500

    Creacion de from en angular desde cero

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.componente';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { BodyComponent } from './componentes/body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/componentes/header/header.componente.ts
@@ -0,0 +1,15 @@
+import { Component } from  '@angular/core';
+import { componentFactoryName } from '@angular/compiler';
+
+
+@Component( {
+selector: 'app-header',
+templateUrl: './header.componente.html'
+  
+
+} )
+
+  
+export class HeaderComponent { 
+
+}
